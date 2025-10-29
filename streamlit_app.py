51f22158da697c4db3f217ab79f50e9f0583763c
Smariner153333 <Smariner6@outlook.com>
2025-10-28 23:18:20 -0700

    Load Keras model for image classification

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,5 +4,6 @@ img = Image.open(picture.png)
 st.image(img, width=150) 
 
 st.title("EcoSave")
+model = tf.keras.models.load_model("keras_model.h5")
 
 st.header("This is an Image Classification App with trash and recycling")
